f2d2cceb9e92e5a8c9c68bba28459ebb3cdbcc3a
Safwan <serajumsafwan25@gmail.com>
2024-01-07 19:38:58 +0600

    mobile Project page Update

--- src/Pages/ProjectPage.jsx
+++ src/Pages/ProjectPage.jsx
@@ -42,23 +42,72 @@ const ProjectPage = () => {
           </div>
           <p className='pl-10'>{projectData.services}</p>
 
-          <div className='p-5' id='thumbnail'>
-            <img src={projectData.thumbnail} ></img>
+          <div className=' p-1 sm:p-5 ' id='thumbnail'>
+            <img src={projectData.thumbnail2} ></img>
           </div>
             
             
-          </section>
-          <section id='projectInfo1' className='relative justify-center
-           flex  mt-20'>
             
-            <div id='ambition' className="w-2/3 ">
-            <h4 className='text-2xl textP font-light font-Satoshi'>Ambition:</h4>
-            <div id='line' className='h-20 rounded-sm w-0.5 bg-black'></div>
+          </section>
+          <section id='prologue' className='relative justify-center
+           flex  mt-10 sm:mt-20'>
+            <div id='prologue' className="w-full  sm:p-0 sm:w-2/3 ">
+              <div className='p-6 sm:p-0'>
+            <h4 className='text-sm sm:text-2xl textP font-light  font-Satoshi'>Prologue:</h4>
+            <div id='line' className='h-20 rounded-sm w-[1px] sm:w-0.5  bg-black'></div>
+            <h1 id='prologue' className='text-3xl mt-2 sm:mt-10 textC font-light sm:text-5xl font-Lora'> 
+            {projectData.prologueHead}
+          
+            </h1>
+            <p id='prologueDescription' className='font-Satoshi textP mt-4 font-light text-sm sm:text-2xl' >Lorem Ipsum is simply dummy text of 
+            the printing and typesetting industry. Lorem Ipsum has been the industry's 
+            standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+             and scrambled it to make a type specimen book.</p>
+             </div>
+             <div className='flex justify-center items-center w-full'>
+            <video alt={projectData.introVideoAlt} muted autoPlay loop  className="sm:w-3/4 w-full p-1 sm:py-10 aspect-video object-cover"src={projectData.introVideo}></video>
+
+            </div>
+              {/* Render video or image based on file type */}
+    {projectData.prologueContent && (
+      <div className='p-1 mt-4'>
+        {projectData.prologueContent.endsWith('.mp4') ? (
+          <video
+            alt={projectData.prologueContent}
+            autoPlay
+            loop
+            muted
+            className='w-screen p-1 sm:py-10 aspect-video object-cover'
+            src={projectData.prologueContent}
+          ></video>
+        ) : (
+          <img
+            className='w-full py-10 aspect-image object-cover'
+            src={projectData.prologueContent}
+            alt={projectData.prologueContent}
+          />
+          
+        )}
+        </div>)}
+      
+
+
+
+          </div>
+
+           </section>
+
+          <section id='projectInfo1' className='w-full  sm:p-0  relative justify-center
+           flex  mt-20'>
+           
+            <div id='ambition' className="p-6 sm:w-2/3 sm:p-0 ">
+            <h4 className='text-sm sm:text-2xl textP font-light  font-Satoshi'>Ambition:</h4>
+            <div id='line' className='h-20 rounded-sm w-[1px] sm:w-0.5 bg-black'></div>
             <h1 id='ambitionHead' className='text-3xl mt-10 textC font-light sm:text-5xl font-Lora'> 
             {projectData.ambitionHead}
           
             </h1>
-            <p id='ambitionDescription' className='font-Satoshi textP mt-4 font-light text-xl sm:text-2xl' >Lorem Ipsum is simply dummy text of 
+            <p id='ambitionDescription' className='font-Satoshi textP mt-4 font-light text-sm sm:text-2xl' >Lorem Ipsum is simply dummy text of 
             the printing and typesetting industry. Lorem Ipsum has been the industry's 
             standard dummy text ever since the 1500s, when an unknown printer took a galley of type
              and scrambled it to make a type specimen book.</p>
@@ -71,14 +120,14 @@ const ProjectPage = () => {
 
           </section>
           <section id='projectInfo2' className='flex flex-col justify-center mt-20 items-center w-[100vw]'>
-          <video alt={projectData.vidAlt1} autoPlay loop  muted className='w-3/4 p-10 aspect-video object-cover ' src={projectData.video1} ></video>
+          <video alt={projectData.vidAlt1} autoPlay loop  muted className='w-full p-1 sm:w-3/4 sm:p-10 aspect-video object-cover ' src={projectData.video1} ></video>
 
-          <div id='challenge' className='w-2/3 font-Satoshi'>
-          <h4 className='text-2xl textP font-light font-Satoshi'>Challenge:</h4>
-          <div id='line' className='h-20 rounded-sm w-0.5 bg-black'></div>
+          <div id='challenge' className= 'w-full p-6 sm:p-0 sm:w-2/3 font-Satoshi'>
+          <h4 className='text-sm sm:text-2xl textP font-light font-Satoshi'>Challenge:</h4>
+          <div id='line' className='h-20 rounded-sm w-[1px] sm:w-0.5 bg-black'></div>
           <h1 id='challengeHead'  className='mt-10 text-3xl textC font-light sm:text-5xl font-Lora'> 
             {projectData.challengeHead}</h1>
-            <p id='ambitionDescription' className='font-Satoshi textP mt-4 font-light text-xl sm:text-2xl' >Lorem Ipsum is simply dummy text of 
+            <p id='ambitionDescription' className='font-Satoshi textP mt-4 font-light text-sm sm:text-2xl' >Lorem Ipsum is simply dummy text of 
             the printing and typesetting industry. Lorem Ipsum has been the industry's 
             standard dummy text ever since the 1500s, when an unknown printer took a galley of type
              and scrambled it to make a type specimen book.</p>
@@ -88,13 +137,13 @@ const ProjectPage = () => {
           </section>
 
           <section id="projectInfo3"  className='flex flex-col justify-center items-center'>
-            <img className=" w-3/4 m-20" src={projectData.image1} alt="" />
-            <div id='approach' className='w-2/3 font-Satoshi'>
-          <h4 className='text-2xl textP font-light font-Satoshi'>Approach:</h4>
-          <div id='line' className='h-20 rounded-sm w-0.5 bg-black'></div>
+            <img className="w-full p-1 sm:p-0 sm:w-3/4 my-10 sm:mt-20" src={projectData.image1} alt="" />
+            <div id='approach' className='w-full sm:p-0 p-6 sm:w-2/3 font-Satoshi'>
+          <h4 className='text-sm sm:text-2xl  textP font-light font-Satoshi'>Approach:</h4>
+          <div id='line' className='h-20 rounded-sm w-[1px] sm:w-0.5 bg-black'></div>
           <h1 id='approachHead'  className='mt-10 text-3xl textC font-light sm:text-5xl font-Lora'> 
             {projectData.approachHead}</h1>
-            <p id='approachDesciption' className='font-Satoshi textP mt-4 font-light text-xl sm:text-2xl' >Lorem Ipsum is simply dummy text of 
+            <p id='approachDesciption' className='font-Satoshi textP  mt-4 font-light text-sm sm:text-2xl' >Lorem Ipsum is simply dummy text of 
             the printing and typesetting industry. Lorem Ipsum has been the industry's 
             standard dummy text ever since the 1500s, when an unknown printer took a galley of type
              and scrambled it to make a type specimen book.</p>
@@ -102,13 +151,13 @@ const ProjectPage = () => {
           </div>
           
             
-            <div id='visualSection1 ' className='flex flex-row justify-center items-center w-[100vw]'>
-            <video alt={projectData.vidAlt2} autoPlay loop  muted className='w-1/3 p-10 aspect-[1/2] object-cover ' src={projectData.video2} ></video>
-            <img className="  w-1/2" src={projectData.image2} alt="" />
+            <div id='visualSection1 ' className='flex flex-col sm:flex-row justify-center items-center w-[100vw]'>
+            <video alt={projectData.vidAlt2} autoPlay loop  muted className='p-6 sm:p-0 sm:w-1/3 sm:p-10 aspect-[1/2] object-cover ' src={projectData.video2} ></video>
+            <img className=" sm:p-0 p-1  sm:w-1/2" src={projectData.image2} alt="" />
             </div>
             <div id='visualSection2 ' className='flex flex-col justify-center items-center w-[100vw]'>
-            <video alt={projectData.vidAlt2} autoPlay loop  muted className='w-2/3 p-10 aspect-video object-cover ' src={projectData.video3} ></video>
-            <img className="  w-1/2" src={projectData.image3} alt="" />
+            <video alt={projectData.vidAlt2} autoPlay loop  muted className=' sm:w-2/3 p-1 sm:p-10 aspect-video object-cover ' src={projectData.video3} ></video>
+            <img className=" p-1 sm:p-0  sm:w-1/2" src={projectData.image3} alt="" />
             </div>
             
           </section>
--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,15 +1,21 @@
 import React, { useRef,useState, useEffect } from 'react';
-import starFallVideo from "../../Assets/StarFall insta reel blue.mp4"
-import starFallPhoto from "../../Assets/Container-Mockup.jpg"
-import starFallImage1 from "../../Assets/Ecostellar v4.jpg"
-import starFallImage2 from "../../Assets/STARFALL MOBILE WEBSITE MOCKUP v1.jpg"
+
 
 import LeicanAraPhoto from "../../Assets/Magazine.png"
 import SvavaPhoto from "../../Assets/Zen.jpg"
 import leicanAraPortrait from "../../Assets/portrait uw.png"
+
+//Starfall Imports
+import starFallVideo from "../../Assets/StarFall insta reel blue.mp4"
+import starFallPhoto from "../../Assets/Container-Mockup.jpg"
+import starFallImage1 from "../../Assets/Ecostellar v4.jpg"
+import starFallImage2 from "../../Assets/STARFALL MOBILE WEBSITE MOCKUP v1.jpg"
 import starFallVideo2 from "../../Assets/Laptop Mockup 1080 v2.mp4"
 import starFallVideo3 from "../../Assets/StarFall Colors 6.mp4" 
-import starFallImage3 from "../../Assets/StarFall Logo Showcase Mockup .jpg"
+import starFallImage3 from "../../Assets/Mockup 3.jpg"
+import starFallImage4 from "../../Assets/StarFall Thumbnail-4.png"
+import starFallIntroVideo from "../../Assets/StarFall Introducing website2.mp4"
+import starFallPrologue from "../../Assets/StarFall Founded Graphic v2.mp4"
  
 
 
@@ -25,6 +31,8 @@ const projects = [
       thumbnail: starFallPhoto,
       
       thumbnailAlt:"image description",
+
+
       projectLink: '/ProjectPage',
       textColor:"text-MainBeige",
   
@@ -38,6 +46,9 @@ const projects = [
 
 
       //Project Page Content
+      thumbnail2:starFallImage4,
+
+      prologueHead:"The year is 2051, ",
       ambitionHead:"To create a brand that inspires people to look beyond the stars and into the future.",
       challengeHead:"To create a revolution",
       approachHead:"The approach of brilliance",
@@ -47,7 +58,9 @@ const projects = [
       image1:starFallImage1,
       image2:starFallImage2,
       image3:starFallImage3,
-
+      
+      introVideo:starFallIntroVideo,
+      prologueContent:starFallPrologue,
       video1:starFallVideo2  ,
       video1Alt:"video description",
       video2:starFallVideo,
