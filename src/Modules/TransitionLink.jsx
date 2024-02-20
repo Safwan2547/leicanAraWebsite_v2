d75e29422134847f7df4f4af61441b298941c735
Safwan <serajumsafwan25@gmail.com>
2024-02-20 14:32:37 -0400

    transition link fixed, used motion one animate

--- src/Modules/TransitionLink.jsx
+++ src/Modules/TransitionLink.jsx
@@ -1,41 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { animate } from 'motion';
 
-const TransitionLink = ({to, children, className}) => {
-  const [opacity, setOpacity] = useState('opacity-0');
-    const [isTransitioning, setIsTransitioning] = useState(false);
-
+const TransitionLink = ({ to, children, className }) => {
+  const [isTransitioning, setIsTransitioning] = useState(false);
   const navigate = useNavigate();
-  useEffect(() => {
-    if (opacity === 'opacity-100') {
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsTransitioning(true);
+
+    // Animate the opacity of the entire page to 0
+    animate(document.documentElement, { opacity: 0 }, { duration: 0.5 });
+
+    // After a short delay, navigate to the new route
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+      navigate(to);
+
+      // After navigation, animate the opacity of the entire page back to 1
       setTimeout(() => {
-        navigate(to);
-        setOpacity('opacity-0');
-      }, 1000);
-    }
-  }, [opacity, navigate, to]);
+        animate(document.documentElement, { opacity: 1 }, { duration: 0.5 });
+      }, 500); // Short delay before animating to ensure the new content is rendered
+    }, 500); // Adjust the delay as needed
+  };
 
+  // Render the link
   return (
-    <>
-      <Link 
-        to={to} 
-        onClick={(e) => {
-          e.preventDefault();
-          setIsTransitioning(true);
-
-          setTimeout(() => setOpacity('opacity-100'), 10);
-          setTimeout(() => setIsTransitioning(false), 5000);
-      }}
-        
-        className={className}>
-        {children}
-      </Link>
-
-      {isTransitioning && 
-        <div className={` fixed top-0 left-0 w-screen h-screen  transition-opacity duration-300 ${opacity}`}></div>
-      }
-    </>
+    <a href={to} onClick={handleClick} className={className}>
+      {children}
+    </a>
   );
 };
 
