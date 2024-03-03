317011a7d3088f4e1925a7d6ac23ec762ecf0b4d
Safwan <serajumsafwan25@gmail.com>
2024-03-03 16:03:31 -0400

    convert to framer motion for nav menu and landing

--- src/Pages/HomePage/landing.jsx
+++ src/Pages/HomePage/landing.jsx
@@ -1,140 +1,51 @@
-import React, { useRef,useState, useEffect } from 'react';
-import logo from '../../Assets/Logo.svg'; // Adjust the path according to your directory structure
-import landingVid from '../../Assets/Website landing 1.mp4'
-import { animate, scroll, inView,timeline,spring,stagger } from "motion"
-import Intro from './Intro';
-import LandingExhibit from '../../Modules/landingExhibit';
-import Frame from '../../Modules/frame';
-import { Parallax,ParallaxLayer } from '@react-spring/parallax';
-
-
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import landingVid from '../../Assets/Website landing 1.mp4';
 
 function LandingPage() {
-  const [isMounted1, setIsMounted1] = useState(false);
-  //This starts the video on the landing page
-  const [showLandingVideo, setShowLandingVideo] = useState(false); // State to control when to show the landing video
+  const controls = useAnimation();
+  const controlsHeader = useAnimation();
 
-  //  delay for the landing video
-  setTimeout(() => {
-    setShowLandingVideo(true);
-  }, 0);
-
-  const select=document.querySelectorAll("span");
-
-
-//This is the animation for the landing page
-  const landingAnimations=[
-   
-    // Final state: set opacity to desired value
-  
-    ['.landingAnimations', { clipPath:"circle(150% at 0% 0)", opacity: 1 }, {duration:1.5, easing: "cubic-bezier(.16,1.17,1,.98)" }],
-    ['.landingAnimations', { opacity: 1, transform: "scale(1)" }, {duration:1.5, easing: "cubic-bezier(.42,0,.13,1.17)" ,at: "<" } ],
-
+  const landingAnimations = [
+    ['.landingAnimations', { clipPath:"circle(150% at 0% 0)", opacity: 1 }, {duration:1.5, ease: "cubic-bezier(.16,1.17,1,.98)" }],
+    ['.landingAnimations', { opacity: 1, scale: 1 }, {duration:1.5, ease: "cubic-bezier(.42,0,.13,1.17)" ,at: "<" }]
   ];
-//animation for the nav bar
-  
 
-    
-  
-// ];
-//This is the intro Animation
-//Must ensure the animation only triggers once when the user first logs in (May need cookies)
+  useEffect(() => {
+    controls.start({ opacity: 1, scale: 1 });
+    controlsHeader.start({ opacity: 1 });
+  }, [controls, controlsHeader]);
 
-// const introAnimate=[
-//   [".intro-Back",{ opacity: 0}, { duration: 0.3,delay:1.5,easeing:["ease-in", "ease-out"] }],
-//   [".intro-Back",{ display:"none"}, {  duration:0.01, delay:0.5  }]
-
-
-  
- 
-
-  function stagger(delay) {
-  return (i) => i * delay;
-}
-
-   useEffect(() => {
-  // timeline(introAnimate, {duration : 0},{
-  //   defaultOptions: { ease: "ease-in"}} )
-
-   timeline(landingAnimations, {delay:0.8},{
-    defaultOptions: {easing:"ease-in-out" },
-  })
-
-  
-  
-  
-   
-    return () => {
-      
-    };
-  }, []);
-  
-  
-
- 
-  
-
-    return (
-      //wrapper
-      <div  className=" ">
-      {/* <Parallax pages={3} ref={parallax} scrolling={true} horizontal={false} > */}
-        
-      <div  className={` transition-opacity overflow-hidden duration-1000 
-       sm:ml-0 overflow-x-hidden flex flex-col 
-       sm:flex-wrap justify-center  sm:items-center   sm:justify-items-start
-       h-screen prose prose-sm lg:prose-xl text-MainBeige relative`}>
-
-
-<div   data-speed="1"  className="  z-1   w-screen h-screen absolute outline-none ">
-          {/* Conditionally render the landing video based on showLandingVideo state */}
-          {showLandingVideo && (
-            <video className="w-full   h-full absolute scale-90 sm:bottom-0 bottom-12  sm:scale-[50%] border-none overflow-hidden object-cover hover:none" controls={false} autoPlay muted loop>
-              <source src={landingVid} type="video/mp4" />
-            </video>
-          )}
-        </div>
-
-        
-
-      
-
-          {/* This is the Mobile view */}       
-          {/* <div className=' flex justify-center  top-2/3 items-center  sm:hidden  absolute '>  
-      <h1  className={`landingAnimations  text-5xl  sm:text-6xl w-3/4 font-Lora font-light text-center  opacity-100 sm:w-1/3  text-NightFall   
-         sm:hidden textC transition duration-200 `}>
-          <span className='textC opacity-0 font-normal text-8xl'>Storytellers</span> <span className='opacity-0 '>for</span> <br></br><span className=' opacity-0'>the</span> <span className=' textC opacity-0 text-8xl'>Visionaries</span>
-          </h1>
-          
-         
-          </div>    */}
-
-
-          {/* This is the Desktop view */}
-
-          {/* <LandingExhibit /> */}
-         
-
-         
-          <div  strength={100} className=' opacity-100  text-NightFall hidden sm:flex  sm:absolute hover:text-LunarTwilight 
-            transition-text duration-200   bottom-[10%] w-2/3 flex-col z-3 perspective-800'>
+  return (
+    <div className="">
+      <div className={`transition-opacity overflow-hidden duration-1000 sm:ml-0 overflow-x-hidden flex flex-col sm:flex-wrap justify-center sm:items-center sm:justify-items-start h-screen prose prose-sm lg:prose-xl text-MainBeige relative`}>
+        <div className="z-1 w-screen h-screen absolute flex justify-center items-center outline-none">
+          <motion.video
+            className="w-full  scale-90 absolute sm:scale-[50%] border-none overflow-hidden object-cover hover:none"
+            controls={false} autoPlay muted loop
             
-          
-          <h1 style={{ clipPath: 'circle(0% at 0% 0)' }} data-speed="6" id='hero_line'  className={`relative mouseParallax landingAnimations scale-[90%] font-extralight tracking-wide textC mb-5 opacity-100 landingItem1 group  text-NightFall font-Lora  sm:text-5xl text-center leading-none`}>
-          <span  className='  textC text-LunarTwilight    opacity-100 font-normal text-8xl' >Storytellers</span> <span className='opacity-100'>for</span> <br></br><span className=' opacity-100'>the</span> <span className=' textC font-normal text-LunarTwilight opacity-100 text-8xl'>Visionaries</span>
-          </h1>
-          
-          
+            transition={{ duration: 3,ease: "circInOut" }}
+            src={landingVid}
+            type="video/mp4"
+          />
+        </div>
 
-          </div >
-          
-         
+        <div strength={100} className='opacity-100 text-NightFall hidden sm:flex sm:absolute hover:text-LunarTwilight transition-text duration-200 bottom-[10%] w-2/3 flex-col z-3 perspective-800'>
+          <motion.h1
+            style={{ clipPath: 'circle(0% at 0% 0)' }}
+            data-speed="6"
+            id='hero_line'
+            className={`relative  landingAnimations scale-[90%] font-extralight tracking-wide textC mb-5 opacity-100 landingItem1 group text-NightFall font-Lora sm:text-5xl text-center leading-none`}
+            initial={{  clipPath: 'circle(0% at 0% 0)', scale: 0.90 }}
+            animate={{ clipPath: 'circle(150% at 0% 0)',scale: 1}}
+            transition={{delay:0.7, duration: 1.5,ease: "circInOut" }}
+          >
+            <span className='textC text-LunarTwilight opacity-100 font-normal text-8xl'>Storytellers</span> <span className='opacity-100'>for</span> <br></br><span className='opacity-100'>the</span> <span className='textC font-normal text-LunarTwilight opacity-100 text-8xl'>Visionaries</span>
+          </motion.h1>
         </div>
-        {/* </Parallax> */}
-        </div>
-        
-    );
-    
-  
-  }
+      </div>
+    </div>
+  );
+}
 
 export default LandingPage;
--- src/Pages/ProjectPage/projectLanding.jsx
+++ src/Pages/ProjectPage/projectLanding.jsx
@@ -1,48 +1,68 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { inView, animate } from 'motion';
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 
 const LandingSection = ({ projectData }) => {
-  const { title, mainHeader,secondaryHeader, thumbnail2 } = projectData;
+  const { title, mainHeader, secondaryHeader, thumbnail2 } = projectData;
 
-  const sectionAnimations = (target,val) => {
-    animate(
-      target.querySelector("img"),
-      { opacity: 1,transform:`scale(${val==1?2:0})` },
-      {  duration: 0.5,delay:0.2, easing:"ease-in-out" }
-    );}
+  const controls = useAnimation();
+  const controlsHeader = useAnimation();
 
-   
-
+  const sectionAnimations = (val) => {
+    controls.start({ opacity: 1, scale: val === 1 ? 2 : 0, duration: 0.5, delay: 0.2, ease: "easeInOut" });
+  };
 
   useEffect(() => {
-    inView("x ", ({ target }) => {
-
-      sectionAnimations(target,1);
-  
-      //when the element leaves the viewport
-  
-      return ()=>{sectionAnimations(target,0);}})
-
-
-  }, []);
+   
+  });
 
   return (
-    <div id='landing' className='w-screen h-[120vh]
-     overflow-hidden flex justify-center items-start'>
-        <div id='landingText' className='w-full z-3 h-full items-center flex text-LunarTwilight  '> 
-        <div id="headerInfo" className=' flex absolute justify-evenly align-baseline   place-items-end border-black '>
-          <h1 data-speed="0" className=' textC text-6xl  sm:text-[10rem] max-w-[75vw] mouseParallax pl-10 text-LunarDawn tracking-wider  font-satoshi-bold'>{secondaryHeader?secondaryHeader:mainHeader}</h1>
+    <motion.div
+      id='landing'
+      animate={{ opacity: 1 }}
+      className='w-screen h-[130vh] overflow-hidden flex justify-center items-start'
+    >
+      <motion.div
+        id='landingText'
+        className='w-full z-3 h-screen items-center flex text-LunarTwilight'
+        initial={{ clipPath:"circle(0% at 0% 0)",scale:0.95 }}
+        animate={{ clipPath:"circle(150% at 0% 0)",scale:1 }}
+        transition={{ duration: 4,ease: "circInOut" }}
+      >
+        <motion.div
+          id="headerInfo"
+          className='flex absolute justify-evenly align-baseline place-items-end border-black'
+         
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <motion.h1
+            data-speed="0"
+            className='textC text-6xl sm:text-[10rem] max-w-[75vw] mouseParallax pl-10 text-LunarDawn tracking-wider font-satoshi-bold'
+           
+            animate={controlsHeader}
+          >
+            {secondaryHeader ? secondaryHeader : mainHeader}
+          </motion.h1>
           <p className='text-2xl hidden text-black overflow-hidden textP align-baseline font-Satoshi font-light'>{title}</p>
-        </div>
-        
-        </div>
-      <div className='w-full h-full z-1 absolute justify-end  flex overflow-hidden items-center' id='thumbnail'>
-        
-        <img data-speed="4" className='w-[50vw] mouseParallax p-12 object-cover sm:h-[85vh]' src={thumbnail2} alt="Project Thumbnail" />
+        </motion.div>
+      </motion.div>
+      <motion.div
+        className='w-full h-full z-1 absolute justify-end flex overflow-hidden items-center'
+        id='thumbnail'
+        animate={controls}
+      >
+        <motion.img
+        initial={{ clipPath:"circle(0% at 0% 0)",scale:0.95 }}
+        animate={{ clipPath:"circle(150% at 100% 0)",scale:1 }}
+        transition={{ duration: 3,ease: "circInOut",delay:1 }}
+          data-speed="4"
+          className='w-[50vw]  p-12 object-cover sm:h-[85vh]'
+          src={thumbnail2}
+          alt="Project Thumbnail"
+        />
         <div id='x'></div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
